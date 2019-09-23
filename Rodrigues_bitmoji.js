1ffc2866c095c88afdaf24c40e23c737ae33738a
Markeemr <54964356+Markeemr@users.noreply.github.com>
2019-09-23 14:10:54 -0400

    Add files via upload

---
+++ Rodrigues_bitmoji.js
@@ -0,0 +1,101 @@
+
+
+background(211, 211, 211);
+
+fill(113, 142, 199);
+rect(83,273,91,85,6);//jean jacket
+rect(225,273,91,85,6);//jean jacket
+triangle(210,287,140,309,128,249);//jean jacket
+triangle(257,304,215,295,270,254);//jean jacket
+
+
+fill(211,211,211);
+arc(197,231,174,122,105,435);//hoodie
+arc(197,231,147,112,105,435);//hoodie
+
+
+fill(222, 175, 129);
+arc(138,194,41,41,43,314);//ear
+arc(141,194,41,41,43,314);//ear
+arc(261,194,41,41,210,492);//ear
+arc(258,194,41,41,210,492);//ear
+
+fill(211, 211, 211);
+rect(168, 264, 60, 95);//sweater
+
+
+
+
+fill(207, 165, 127);
+ellipse(200,200,138,163);//head
+bezier(193,210,234,208,195,183,194,188);//nose
+fill(3, 3, 3);
+arc(199,271,41,41,339,562);//ear
+
+
+
+fill(255, 0, 0);
+
+
+fill(242, 242, 242);
+ellipse(173,175,24,19);//eyes
+ellipse(227,175,24,19);//eyes
+fill(107, 60, 14);
+ellipse(173,175,8,12);//eyes
+ellipse(227,175,8,12);//eyes
+fill(0, 0, 0);
+noFill();
+strokeWeight(5);
+arc(173,199,80,80,248,294);//eyebrows can i make them fuller?
+arc(228,199,80,80,248,294);//eyebrows
+strokeWeight(3);
+arc(192,264,69,80,241,278);//mustache
+arc(202,264,80,80,269,299);//mustache
+strokeWeight(1);
+
+
+
+fill(224, 112, 112);
+ellipse(198,247,27,18);
+fill(240, 235, 235);
+line(186,243,208,243);//upperlip
+line(188,251,207,251);//lowerlip
+rect(186,247,23,3,1);
+
+//text
+textSize(16);
+fill(12, 122, 19);
+text("Stay", 183, 321);
+text("Silent", 178, 339);
+textSize(25);
+text("MR", 263, 321);
+
+fill(0, 0, 0);
+ellipse(189,138,9,38);//hair
+ellipse(209,138,9,38);//hair
+ellipse(180,136,9,38);//hair
+ellipse(219,137,9,38);//hair
+fill(70, 255, 33);
+noStroke();
+arc(202, 144, 145, 120, 164, 379);//beanie
+fill(255, 255, 255);
+rect(185,97,34,38,1);//logo
+fill(19, 184, 7);
+rect(188,115,28,8,1);//logo
+fill(247, 241, 62);
+textSize(18);//logo
+text("C", 188, 112);
+fill(255, 0, 0);
+textSize(18);
+text("C", 202, 112);
+fill(0, 0, 0);
+textSize(8);
+text("CURED", 188, 133);
+stroke(0, 0, 0);
+fill(211, 211, 211);
+ellipse(192,298,17,11);//string
+ellipse(204,298,17,11);//string
+
+
+
+
